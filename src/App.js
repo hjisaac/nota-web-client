e234cebb07fe346b5a18f43a30d41f119806253d
Isaac H. J. Houngue <hjisaac>
2021-04-01 20:36:17 +0100

    Store logged user information in the browser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "apollo-link-context";
 
 import GlobalStyle from "./components/GlobalStyle";
 import Pages from "./pages";
 
 
 const uri = process.env.API_URI;
+const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
 
+// check for the token and return the headers to the context
+
+const authLink = setContext(
+    (_, { headers }) => {
+        return {
+            headers: {
+                ...headers,
+                autorization: localStorage.getItem("token") || ""
+            }
+        };
+    }
+);
+
 const client = new ApolloClient({
-    uri,
+    link: authLink.concat(httpLink),
     cache,
+    resolvers: {},
     connectToDevTools:true
 });
 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,16 +1,106 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useMutation, useApolloClient, gql } from "@apollo/client";
+
+import Button from "../components/Button";
+
+const signUpUser = gql`
+    mutation signUp($email: String!, $username: String!, $password: String!) {
+        signUp(email: $email, username: $username, password: $password)
+    }
+`;
+
+const Wrapper = styled.div`
+    border: 1px solid #f5f4f0;
+    max-width: 500px;
+    padding: 1em;
+    margin: 0 auto;
+`;
+
+const Form = styled.form`
+    label, input {
+        display: block;
+        line-height: 2em;
+    }
+
+    input {
+        width: 100%;
+        margin-bottom: 1em;
+    }
+`;
 
 
 const SignUp = props => {
+    // set the default state of the form
+    const [values, setValues] = useState();
+
+    // update the state when a user types in the form
+    const onChange = (event) => {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value
+        });
+    }
+
     useEffect(() => {
         // update the document title
         document.title = "Sign Up | Nota";
     });
 
+    const [ signUp, { loading, error }] = useMutation(signUpUser, {
+        onCompleted: data => {
+            // console.log the jwt when the mutation is complete
+            localStorage.setItem("token",  data.signUp);
+            console.log(localStorage.getItem("token"));
+            props.history.push("/");
+        }
+    });
+
     return (
-        <div>
-            <p>Sign Up</p>
-        </div>
+        <Wrapper>
+            <h2>Sign Up</h2>
+            <Form 
+                onSubmit={event => {
+                    event.preventDefault();
+                    signUp({
+                        variables: {
+                            ...values
+                        }
+                    });
+            }}> 
+                <label htmlFor="username"> Username: </label>
+                <input 
+                    required 
+                    type="text" 
+                    id="username" 
+                    name="username" 
+                    placeholder="Username"
+                    onChange={onChange}
+                />
+
+                <label htmlFor="email"> Email: </label>
+                <input 
+                    required 
+                    type="email" 
+                    id="email" 
+                    name="email" 
+                    placeholder="Email" 
+                    onChange={onChange}
+                />
+
+                <label htmlFor="password"> Password: </label>
+                <input 
+                    required 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Password" 
+                    onChange={onChange}
+                />
+
+                <Button type="submit">Submit</Button>
+            </Form>
+        </Wrapper>
     );
 };
 
